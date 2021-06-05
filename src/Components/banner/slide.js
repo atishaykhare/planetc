14501fac26e34afb28d53c649b036ade0fb9d690
atishay <atishay@dono.consulting>
2021-06-05 23:32:24 +0530

    initial-commit

---
+++ src/Components/banner/slide.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import s from './Carousel.module.css'
+import PlayButton from "../playButton/playButton";
+
+const Slide = (props) => {
+    const {img} = props;
+    return (
+       <div className={s.container}>
+           <img src={img} className={s.slideImg}/>
+           <div className={s.sliderText}>
+               <h5>Star Wars The Force Awakens</h5>
+           </div>
+           <div className={s.playBtn}>
+               <PlayButton/>
+           </div>
+       </div>
+    );
+};
+
+export default Slide;
